a2376e4fdc93ee7abc6067aed3e9c00d75537fc3
mstruzak <mstruzak@andrew.cmu.edu>
2026-03-30 01:46:12 +0000

    Deploying to gh-pages from @ mstruzak/mstruzak.github.io@6e6d10175befbee84f61f7238bae5539b51efc90 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,7 +18,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "false",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
